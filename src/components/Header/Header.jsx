cc058e237edf8453e46da78d9ef5a9fa43ab101d
enamulhimel <mhasan173013@bscse.uiu.ac.bd>
2023-11-09 04:27:20 +0600

    home page develop

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Row, Button } from 'reactstrap';
 import { NavLink, Link } from 'react-router-dom';
 import logo from '../../assets/images/logo.png';
+import './header.css';
 
 const nav_links = [
   {
@@ -35,12 +36,32 @@ const Header = () => {
               <ul className="menu d-flex align-items-center gap-5">
                 {nav_links.map((item, index) => (
                   <li className="nav_item" key={index}>
-                    <NavLink to={item.path}>{item.display}</NavLink>
+                    <NavLink
+                      to={item.path}
+                      className={(navClass) =>
+                        navClass.isActive ? 'active__link' : ' '
+                      }
+                    >
+                      {item.display}
+                    </NavLink>
                   </li>
                 ))}
               </ul>
             </div>
             {/* Menu End */}
+            <div className="nav_right d-flex align-items-center gap-4">
+              <div className="nav_btns d-flex align-items-center gap-4">
+                <Button className="btn secondary__btn">
+                  <Link to="/login">Login</Link>
+                </Button>
+                <Button className="btn primary__btn">
+                  <Link to="/register">Register</Link>
+                </Button>
+              </div>
+              <span className="mobile-menu">
+                <i class="ri-menu-line"></i>
+              </span>
+            </div>
           </div>
         </Row>
       </Container>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,60 @@
 import React from 'react';
+import '../style/Home.css';
+import { Container, Row, Col } from 'reactstrap';
+import Subtitle from '../shared/Subtitle';
+import heroImg01 from '../assets/images/hero-img01.jpg';
+import heroImg02 from '../assets/images/hero-img02.jpg';
+import heroVideo from '../assets/images/hero-video.mp4';
+import worldImg from '../assets/images/world.png';
+import SearchBar from '../shared/SearchBar';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      {/* hero section start */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6">
+              <div className="hero__content">
+                <div className="hero__subtitle d-flex align-items-center">
+                  <Subtitle subtitle={'know before you go'} />
+                  <img src={worldImg} alt="" />
+                </div>
+                <h1>
+                  Traveling opens the door to creating{' '}
+                  <span className="highlight">memories</span>
+                </h1>
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Delectus, accusantium laboriosam sequi culpa quos maxime,
+                  libero voluptas porro harum quo voluptates, possimus cum ad
+                  reprehenderit quisquam at facere consequuntur incidunt.
+                </p>
+              </div>
+            </Col>
+            <Col lg="2">
+              <div className="hero__img-box">
+                <img src={heroImg01} alt="" />
+              </div>
+            </Col>
+            <Col lg="2">
+              <div className="hero__img-box mt-4">
+                <video src={heroVideo} alt="" controls />
+              </div>
+            </Col>
+            <Col lg="2">
+              <div className="hero__img-box mt-5">
+                <img src={heroImg02} alt="" />
+              </div>
+            </Col>
+            <SearchBar />
+          </Row>
+        </Container>
+      </section>
+      {/* hero section End */}
+    </>
+  );
 };
 
 export default Home;
